39f4731334d6ac089ad0d8bd697fad45385fa5b6
Ratchakij-ARKinsights <p.ratchakij@ark-insights.com>
2023-09-09 13:00:12 +0700

    create seeder

---
+++ seeders/20230908041810-seed-users.js
@@ -0,0 +1,40 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          first_name: "John",
+          last_name: "Doe",
+          email: "john@gmail.com",
+          password: "123456",
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("users", null, {});
+  },
+};
